7829badab7c9380aa574e06fef4f044aa14fdee6
Audomsak_Phr <audomsak.p@kkumail.com>
2026-09-08 10:39:25 +0000

    add Page Config

--- indohotel/app.py
+++ indohotel/app.py
@@ -3,3 +3,9 @@ import duckdb
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+
+st.set_page_config(
+    page_title="IndoHotel Executive Analytics",
+    page_icon="🏨",
+    layout="wide"
+)
